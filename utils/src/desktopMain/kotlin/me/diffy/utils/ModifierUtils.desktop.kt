f32cf64276ebe7da839ab2d7f7b2605af2c78eb7
DeFerence3 <aktr12312@gmail.com>
2025-06-18 12:17:37 +0530

    fixed project not working oob

--- utils/src/desktopMain/kotlin/me/diffy/utils/ModifierUtils.desktop.kt
+++ utils/src/desktopMain/kotlin/me/diffy/utils/ModifierUtils.desktop.kt
@@ -1,5 +1,19 @@
 package me.diffy.utils
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.isCtrlPressed
+import androidx.compose.ui.input.pointer.onPointerEvent
 
-actual inline fun Modifier.onCtrlClick(crossinline onClick: () -> Unit): Modifier  = this.then(Modifier)
+@OptIn(ExperimentalComposeUiApi::class)
+actual inline fun Modifier.onCtrlClick(crossinline onClick: () -> Unit): Modifier {
+    return this.then(Modifier.onPointerEvent(PointerEventType.Press) {
+        val event = it.changes.first()
+        if (event.pressed && currentEvent.button == PointerButton.Primary && it.keyboardModifiers.isCtrlPressed) {
+            onClick()
+            event.consume()
+        }
+    })
+}
